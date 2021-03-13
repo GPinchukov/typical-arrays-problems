43fb0e92b73a54a5e0b0a0a1d29c4d6c72eeb241
GPinchukov <pinchukov000@gmail.com>
2021-03-13 21:34:21 +0300

    ssss

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 
 exports.min = function min(array) {
     if (typeof array !== 'undefined' && array.length > 0) {
-        let min = array[0];
+        let mn = array[0];
         for (let i of array) {
-            if (i < min) {
-                min = i;
+            if (i < mn) {
+                mn = i;
             }
         }
-        return min;
+        return mn;
     }
     return 0;
 }
 
 exports.avg = function avg(array) {
     if (typeof array !== 'undefined' && array.length > 0) {
-        let sum = 0;
+        let sm = 0;
         for (let i of array) {
-            sum += i;
+            sm += i;
         }
-        return (sum / array.length).toFixed(2);
+        return (sm / array.length).toFixed(2);
     }
     return 0;
 }
 
 exports.max = function max(array) {
     if (typeof array !== 'undefined' && array.length > 0) {
-        let max = 0;
+        let mx = 0;
         for (let i of array) {
-            if (i > max) {
-                max = i;
+            if (i > mx) {
+                mx = i;
             }
         }
-        return max;
+        return mx;
     }
     return 0;
 }
